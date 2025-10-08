5d793815be2a49f7c80077f2de34f5b4da19b084
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2025-10-08 09:54:42 -0400

    Improve file names

--- server/update_sql_functions.sh
+++ server/update_sql_functions.sh
@@ -8,20 +8,23 @@ SQL_FUNCTIONS_FILE="functions.sql"
 DB_NAME="osm"
 
 # Use the latest lists instead of the lists at import since all tags are present in the db, we're just filtering 
-JSONB_KEYS=$(grep -v '^$' "$APP_DIR/helper_data/output_keys.txt" | sed "s/.*/'&'/" | paste -sd, -)
-JSONB_PREFIXES=$(grep -v '^$' "$APP_DIR/helper_data/output_key_prefixes.txt" | awk '{print "OR key LIKE \x27" $0 "%\x27"}' | paste -sd' ' -)
+JSONB_KEYS=$(grep -v '^$' "$APP_DIR/schema_data/attribute_keys.txt" | sed "s/.*/'&'/" | paste -sd, -)
+JSONB_PREFIXES=$(grep -v '^$' "$APP_DIR/schema_data/attribute_key_prefixes.txt" | awk '{print "OR key LIKE \x27" $0 "%\x27"}' | paste -sd' ' -)
 
-FIELD_DEFS="$(grep -v '^$' "$APP_DIR/helper_data/output_keys.txt" | sed 's/.*/"&":"String"/' | paste -sd, -)"
-FIELD_DEFS="$FIELD_DEFS,$(grep -v '^$' "$APP_DIR/helper_data/output_key_prefixes.txt" | sed 's/.*/"&\*":"String"/' | paste -sd, -)"
+FIELD_DEFS="$(grep -v '^$' "$APP_DIR/schema_data/attribute_keys.txt" | sed 's/.*/"&":"String"/' | paste -sd, -)"
+FIELD_DEFS="$FIELD_DEFS,$(grep -v '^$' "$APP_DIR/schema_data/attribute_key_prefixes.txt" | sed 's/.*/"&\*":"String"/' | paste -sd, -)"
 
-LOW_ZOOM_LINE_JSONB_KEY_MAPPINGS="$(grep -v '^$' "$APP_DIR/helper_data/low_zoom_line_keys.txt" | sed "s/.*/'&', tags->'&'/" | paste -sd, -)"
-LOW_ZOOM_AREA_JSONB_KEY_MAPPINGS="$(grep -v '^$' "$APP_DIR/helper_data/low_zoom_area_keys.txt" | sed "s/.*/'&', tags->'&'/" | paste -sd, -)"
+LOW_ZOOM_LINE_JSONB_KEYS=$(grep -v '^$' "$APP_DIR/schema_data/attribute_keys_low_zoom_line.txt" | sed "s/.*/'&'/" | paste -sd, -)
+LOW_ZOOM_LINE_JSONB_PREFIXES=$(grep -v '^$' "$APP_DIR/schema_data/attribute_key_prefixes_low_zoom_line.txt" | awk '{print "OR key LIKE \x27" $0 "%%\x27"}' | paste -sd' ' -)
+
+LOW_ZOOM_AREA_JSONB_KEY_MAPPINGS="$(grep -v '^$' "$APP_DIR/schema_data/attribute_keys_low_zoom_area.txt" | sed "s/.*/'&', tags->'&'/" | paste -sd, -)"
 
 SQL_CONTENT=$(<"$SQL_FUNCTIONS_FILE")
 SQL_CONTENT=${SQL_CONTENT//\{\{JSONB_KEYS\}\}/$JSONB_KEYS}
 SQL_CONTENT=${SQL_CONTENT//\{\{JSONB_PREFIXES\}\}/$JSONB_PREFIXES}
 SQL_CONTENT=${SQL_CONTENT//\{\{FIELD_DEFS\}\}/$FIELD_DEFS}
-SQL_CONTENT=${SQL_CONTENT//\{\{LOW_ZOOM_LINE_JSONB_KEY_MAPPINGS\}\}/$LOW_ZOOM_LINE_JSONB_KEY_MAPPINGS}
+SQL_CONTENT=${SQL_CONTENT//\{\{LOW_ZOOM_LINE_JSONB_KEYS\}\}/$LOW_ZOOM_LINE_JSONB_KEYS}
+SQL_CONTENT=${SQL_CONTENT//\{\{LOW_ZOOM_LINE_JSONB_PREFIXES\}\}/$LOW_ZOOM_LINE_JSONB_PREFIXES}
 SQL_CONTENT=${SQL_CONTENT//\{\{LOW_ZOOM_AREA_JSONB_KEY_MAPPINGS\}\}/$LOW_ZOOM_AREA_JSONB_KEY_MAPPINGS}
 
 sudo -u postgres psql "$DB_NAME" -v ON_ERROR_STOP=1 <<< "$SQL_CONTENT"

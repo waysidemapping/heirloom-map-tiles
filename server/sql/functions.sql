cfeff56a837b4c007ccc1caf6de322a64d9a35d0
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2025-10-28 12:40:05 -0400

    Bifurcate points function for z < 12 and z >=12.  Dynamically include small points based on estimated number of points in tile area

--- server/sql/functions.sql
+++ server/sql/functions.sql
@@ -370,142 +370,276 @@ CREATE OR REPLACE FUNCTION function_get_line_features(z integer, env_geom geomet
   END;
 $$;
 
-CREATE OR REPLACE FUNCTION function_get_point_features(z integer, env_geom geometry, min_area real, max_area real, min_rel_extent real, max_rel_extent real)
+CREATE OR REPLACE FUNCTION function_get_point_features(z integer, env_geom geometry, wide_env_geom geometry, min_area real, max_area real, min_rel_extent real, max_rel_extent real)
   RETURNS TABLE(_id int8, _tags jsonb, _geom geometry, _area_3857 real, _osm_type text, _relation_ids int8[])
-  LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE
+  LANGUAGE plpgsql IMMUTABLE STRICT PARALLEL SAFE
   AS $$
-    WITH
-    small_points AS (
-        SELECT id, tags, geom, NULL::real AS area_3857, true AS is_node_or_explicit_area, 'n' AS osm_type FROM node
-        WHERE geom && env_geom
-      UNION ALL
-        SELECT id, tags, label_point AS geom, area_3857, is_explicit_area AS is_node_or_explicit_area, 'w' AS osm_type FROM way_no_explicit_line
-        WHERE label_point && env_geom
-          AND area_3857 <= min_area
-      UNION ALL
-        SELECT id, tags, label_point AS geom, area_3857, true AS is_node_or_explicit_area, 'r' AS osm_type FROM area_relation
-        WHERE label_point && env_geom
-          AND area_3857 <= min_area
-    ),
-    filtered_small_points AS (
-      SELECT * FROM small_points
-      WHERE (
-        tags @> 'place => continent'
-        OR tags @> 'place => ocean'
-        OR tags @> 'place => sea'
-        OR tags @> 'place => country'
-        OR tags @> 'place => state'
-        OR tags @> 'place => province'
-      ) OR (
-        (
-          tags @> 'place => city'
+    BEGIN
+    IF z < 12 THEN
+      RETURN QUERY
+      WITH
+      small_points AS (
+          SELECT id, tags, geom, NULL::real AS area_3857, true AS is_node_or_explicit_area, 'n' AS osm_type FROM node
+          WHERE geom && env_geom
+        UNION ALL
+          SELECT id, tags, label_point AS geom, area_3857, is_explicit_area AS is_node_or_explicit_area, 'w' AS osm_type FROM way_no_explicit_line
+          WHERE label_point && env_geom
+            AND area_3857 <= min_area
+        UNION ALL
+          SELECT id, tags, label_point AS geom, area_3857, true AS is_node_or_explicit_area, 'r' AS osm_type FROM area_relation
+          WHERE label_point && env_geom
+            AND area_3857 <= min_area
+      ),
+      low_zoom_small_points AS (
+        SELECT * FROM small_points
+        WHERE (
+          tags @> 'place => continent'
+          OR tags @> 'place => ocean'
+          OR tags @> 'place => sea'
+          OR tags @> 'place => country'
+          OR tags @> 'place => state'
+          OR tags @> 'place => province'
+        ) OR (
+          (
+            tags @> 'place => city'
+          )
+          AND z >= 4
+        ) OR (
+          (
+            tags @> 'place => town'
+          )
+          AND z >= 6
+        ) OR (
+          (
+            tags @> 'place => village'
+          )
+          AND z >= 7
+        ) OR (
+          (
+            tags @> 'place => hamlet'
+            OR tags @> 'natural => peak'
+            OR tags @> 'natural => volcano'
+          )
+          AND z >= 8
+        ) OR (
+          (
+            tags @> 'place => locality'
+            OR tags @> 'public_transport => station'
+            OR tags @> 'aeroway => aerodrome'
+            OR tags @> 'highway => motorway_junction'
+          )
+          AND z >= 9
         )
-        AND z >= 4
-      ) OR (
-        (
-          tags @> 'place => town'
+      ),
+      large_points AS (
+          SELECT id, tags, label_point AS geom, area_3857, is_explicit_area AS is_node_or_explicit_area, 'w' AS osm_type
+          FROM way_no_explicit_line
+          WHERE label_point && env_geom
+            AND area_3857 > min_area
+            AND area_3857 < max_area
+        UNION ALL
+          SELECT id, tags, label_point AS geom, area_3857, true AS is_node_or_explicit_area, 'r' AS osm_type
+          FROM area_relation
+          WHERE label_point && env_geom
+            AND area_3857 > min_area
+            AND area_3857 < max_area
+      ),
+      filtered_large_points AS (
+        SELECT * FROM large_points
+        WHERE (
+          tags ?| ARRAY['advertising', 'amenity', 'boundary', 'building', 'club', 'craft', 'education', 'emergency', 'golf', 'healthcare', 'historic', 'indoor', 'information', 'landuse', 'leisure', 'man_made', 'miltary', 'office', 'place', 'playground', 'public_transport', 'shop', 'tourism']
+        ) OR (
+          tags ? 'natural'
+          AND NOT tags @> 'natural => coastline'
+        ) OR (
+          tags ?| ARRAY['aerialway', 'aeroway', 'barrier', 'highway', 'power', 'railway', 'telecom', 'waterway']
+          AND is_node_or_explicit_area
         )
-        AND z >= 6
-      ) OR (
+      ),
+      route_centerpoints AS (
+        SELECT
+          id,
+          -- don't include tags since we're going to infill them on the client 
+          '{}'::jsonb AS tags,
+          bbox_centerpoint_on_surface AS geom,
+          NULL::real AS area_3857,
+          'r' AS osm_type,
+          ARRAY[id] AS relation_ids
+        FROM non_area_relation
+        WHERE bbox_centerpoint_on_surface && env_geom
+          AND (
+            (tags @> 'type => route' AND tags ? 'route')
+            OR tags @> 'type => waterway'
+          )
+          AND bbox_diagonal_length > min_rel_extent
+          AND bbox_diagonal_length < max_rel_extent
+          AND z >= 4
+      ),
+      all_points AS (
+          SELECT id, tags::jsonb, geom, area_3857, osm_type, NULL::int8[] AS relation_ids FROM low_zoom_small_points
+        UNION ALL
+          SELECT id, tags::jsonb, geom, area_3857, osm_type, NULL::int8[] AS relation_ids FROM filtered_large_points
+        UNION ALL
+          SELECT id, tags::jsonb, geom, area_3857, osm_type, relation_ids FROM route_centerpoints
+      )
+      SELECT
+        id,
         (
-          tags @> 'place => village'
-        )
-        AND z >= 7
-      ) OR (
-        (
-          tags @> 'place => hamlet'
+          SELECT jsonb_object_agg(key, value)
+          FROM jsonb_each(tags)
+          WHERE key IN ({{POINT_KEY_LIST}}) {{POINT_KEY_PREFIX_LIKE_STATEMENTS}}
+        ) AS tags,
+        geom,
+        area_3857,
+        osm_type,
+        relation_ids
+      FROM all_points
+      ;
+    ELSE
+      RETURN QUERY
+      WITH
+      small_points AS (
+          SELECT id, tags, geom, NULL::real AS area_3857, true AS is_node_or_explicit_area, 'n' AS osm_type FROM node
+          WHERE geom && env_geom
+        UNION ALL
+          SELECT id, tags, label_point AS geom, area_3857, is_explicit_area AS is_node_or_explicit_area, 'w' AS osm_type FROM way_no_explicit_line
+          WHERE label_point && env_geom
+            AND area_3857 <= min_area
+        UNION ALL
+          SELECT id, tags, label_point AS geom, area_3857, true AS is_node_or_explicit_area, 'r' AS osm_type FROM area_relation
+          WHERE label_point && env_geom
+            AND area_3857 <= min_area
+      ),
+      low_zoom_small_points AS (
+        SELECT * FROM small_points
+        WHERE
+          tags @> 'place => continent'
+          OR tags @> 'place => ocean'
+          OR tags @> 'place => sea'
+          OR tags @> 'place => country'
+          OR tags @> 'place => state'
+          OR tags @> 'place => province'
+          OR tags @> 'place => city'
+          OR tags @> 'place => town'
+          OR tags @> 'place => village'
+          OR tags @> 'place => hamlet'
           OR tags @> 'natural => peak'
           OR tags @> 'natural => volcano'
-        )
-        AND z >= 8
-      ) OR (
-        (
-          tags @> 'place => locality'
+          OR tags @> 'place => locality'
           OR tags @> 'public_transport => station'
           OR tags @> 'aeroway => aerodrome'
           OR tags @> 'highway => motorway_junction'
+      ),
+      ranked_small_points AS (
+        SELECT *,
+          -- Assume that a feature with a name (e.g. park, business, artwork) is more important one without
+          -- (e.g. crossing, pole, gate). Features linked to Wikidata items are assumed to be notable regardless
+          (tags ? 'name' OR tags ? 'wikidata') AS is_notable
+        FROM small_points
+        WHERE 
+          tags ?| ARRAY['advertising', 'amenity', 'boundary', 'club', 'craft', 'education', 'emergency', 'golf', 'healthcare', 'historic', 'indoor', 'information', 'landuse', 'leisure', 'man_made', 'miltary', 'office', 'place', 'playground', 'public_transport', 'shop', 'tourism']
+          OR (
+            tags ? 'building'
+            AND (tags ? 'name' OR tags ? 'wikidata' OR z >= 15)
+          ) OR (
+            tags ? 'natural'
+            AND NOT tags @> 'natural => coastline'
+          ) OR (
+            tags ?| ARRAY['aerialway', 'aeroway', 'barrier', 'highway', 'power', 'railway', 'telecom', 'waterway']
+            AND is_node_or_explicit_area
+          )
+      ),
+      -- Count the number of point-like features in this region. We use a region larger than the tile itself
+      -- in order to try and avoid sharp visual cutoffs at tile bounds. For performance, this is only an estimate
+      points_in_region AS (
+          SELECT count(id) AS total FROM node
+          WHERE geom && wide_env_geom
+        UNION ALL
+          SELECT count(id) AS total FROM way_no_explicit_line
+          WHERE label_point && wide_env_geom
+        UNION ALL
+          SELECT count(id) AS total FROM area_relation
+          WHERE label_point && wide_env_geom
+      ),
+      point_region_stats AS (
+        SELECT sum(total) AS regional_point_count FROM points_in_region
+      ),
+      -- Only include small points if we don't think they will make the tile too big
+      reduced_small_points AS (
+          SELECT id, tags, geom, area_3857, osm_type
+          FROM ranked_small_points, point_region_stats
+          WHERE regional_point_count < 150000
+            -- Include only notable features unless we have room to spare
+            AND (is_notable OR regional_point_count < 75000)
+      ),
+      large_points AS (
+          SELECT id, tags, label_point AS geom, area_3857, is_explicit_area AS is_node_or_explicit_area, 'w' AS osm_type
+          FROM way_no_explicit_line
+          WHERE label_point && env_geom
+            AND area_3857 > min_area
+            AND area_3857 < max_area
+        UNION ALL
+          SELECT id, tags, label_point AS geom, area_3857, true AS is_node_or_explicit_area, 'r' AS osm_type
+          FROM area_relation
+          WHERE label_point && env_geom
+            AND area_3857 > min_area
+            AND area_3857 < max_area
+      ),
+      filtered_large_points AS (
+        SELECT * FROM large_points
+        WHERE (
+          tags ?| ARRAY['advertising', 'amenity', 'boundary', 'building', 'club', 'craft', 'education', 'emergency', 'golf', 'healthcare', 'historic', 'indoor', 'information', 'landuse', 'leisure', 'man_made', 'miltary', 'office', 'place', 'playground', 'public_transport', 'shop', 'tourism']
+        ) OR (
+          tags ? 'natural'
+          AND NOT tags @> 'natural => coastline'
+        ) OR (
+          tags ?| ARRAY['aerialway', 'aeroway', 'barrier', 'highway', 'power', 'railway', 'telecom', 'waterway']
+          AND is_node_or_explicit_area
         )
-        AND z >= 9
-      ) OR (
-        tags ?| ARRAY['advertising', 'amenity', 'boundary', 'building', 'club', 'craft', 'education', 'emergency', 'golf', 'healthcare', 'historic', 'indoor', 'information', 'landuse', 'leisure', 'man_made', 'miltary', 'office', 'place', 'playground', 'public_transport', 'shop', 'tourism']
-        AND z >= 12
-        AND (tags ? 'name' OR tags ? 'wikidata' OR z >= 15)
-      ) OR (
-        tags ? 'natural'
-        AND NOT tags @> 'natural => coastline'
-        AND z >= 12
-        AND (tags ? 'name' OR tags ? 'wikidata' OR z >= 15)
-      ) OR (
-        tags ?| ARRAY['aerialway', 'aeroway', 'barrier', 'highway', 'power', 'railway', 'telecom', 'waterway']
-        AND is_node_or_explicit_area
-        AND z >= 12
-        AND (tags ? 'name' OR tags ? 'wikidata' OR z >= 15)
+      ),
+      route_centerpoints AS (
+        SELECT
+          id,
+          -- don't include tags since we're going to infill them on the client 
+          '{}'::jsonb AS tags,
+          bbox_centerpoint_on_surface AS geom,
+          NULL::real AS area_3857,
+          'r' AS osm_type,
+          ARRAY[id] AS relation_ids
+        FROM non_area_relation
+        WHERE bbox_centerpoint_on_surface && env_geom
+          AND (
+            (tags @> 'type => route' AND tags ? 'route')
+            OR tags @> 'type => waterway'
+          )
+          AND bbox_diagonal_length > min_rel_extent
+          AND bbox_diagonal_length < max_rel_extent
+          AND z >= 4
+      ),
+      all_points AS (
+          SELECT id, tags::jsonb, geom, area_3857, osm_type, NULL::int8[] AS relation_ids FROM low_zoom_small_points
+        UNION ALL
+          SELECT id, tags::jsonb, geom, area_3857, osm_type, NULL::int8[] AS relation_ids FROM reduced_small_points
+        UNION ALL
+          SELECT id, tags::jsonb, geom, area_3857, osm_type, NULL::int8[] AS relation_ids FROM filtered_large_points
+        UNION ALL
+          SELECT id, tags::jsonb, geom, area_3857, osm_type, relation_ids FROM route_centerpoints
       )
-    ),
-    large_points AS (
-        SELECT id, tags, label_point AS geom, area_3857, is_explicit_area AS is_node_or_explicit_area, 'w' AS osm_type
-        FROM way_no_explicit_line
-        WHERE label_point && env_geom
-          AND area_3857 > min_area
-          AND area_3857 < max_area
-      UNION ALL
-        SELECT id, tags, label_point AS geom, area_3857, true AS is_node_or_explicit_area, 'r' AS osm_type
-        FROM area_relation
-        WHERE label_point && env_geom
-          AND area_3857 > min_area
-          AND area_3857 < max_area
-    ),
-    filtered_large_points AS (
-      SELECT * FROM large_points
-      WHERE (
-        tags ?| ARRAY['advertising', 'amenity', 'boundary', 'building', 'club', 'craft', 'education', 'emergency', 'golf', 'healthcare', 'historic', 'indoor', 'information', 'landuse', 'leisure', 'man_made', 'miltary', 'office', 'place', 'playground', 'public_transport', 'shop', 'tourism']
-      ) OR (
-        tags ? 'natural'
-        AND NOT tags @> 'natural => coastline'
-      ) OR (
-        tags ?| ARRAY['aerialway', 'aeroway', 'barrier', 'highway', 'power', 'railway', 'telecom', 'waterway']
-        AND is_node_or_explicit_area
-      )
-    ),
-    route_centerpoints AS (
       SELECT
         id,
-        -- don't include tags since we're going to infill them on the client 
-        '{}'::jsonb AS tags,
-        bbox_centerpoint_on_surface AS geom,
-        NULL::real AS area_3857,
-        'r' AS osm_type,
-        ARRAY[id] AS relation_ids
-      FROM non_area_relation
-      WHERE bbox_centerpoint_on_surface && env_geom
-        AND (
-          (tags @> 'type => route' AND tags ? 'route')
-          OR tags @> 'type => waterway'
-        )
-        AND bbox_diagonal_length > min_rel_extent
-        AND bbox_diagonal_length < max_rel_extent
-        AND z >= 4
-    ),
-    all_points AS (
-        SELECT id, tags::jsonb, geom, area_3857, osm_type, NULL::int8[] AS relation_ids FROM filtered_small_points
-      UNION ALL
-        SELECT id, tags::jsonb, geom, area_3857, osm_type, NULL::int8[] AS relation_ids FROM filtered_large_points
-      UNION ALL
-        SELECT id, tags::jsonb, geom, area_3857, osm_type, relation_ids FROM route_centerpoints
-    )
-    SELECT
-      id,
-      (
-        SELECT jsonb_object_agg(key, value)
-        FROM jsonb_each(tags)
-        WHERE key IN ({{POINT_KEY_LIST}}) {{POINT_KEY_PREFIX_LIKE_STATEMENTS}}
-      ) AS tags,
-      geom,
-      area_3857,
-      osm_type,
-      relation_ids
-    FROM all_points
-    ;
+        (
+          SELECT jsonb_object_agg(key, value)
+          FROM jsonb_each(tags)
+          WHERE key IN ({{POINT_KEY_LIST}}) {{POINT_KEY_PREFIX_LIKE_STATEMENTS}}
+        ) AS tags,
+        geom,
+        area_3857,
+        osm_type,
+        relation_ids
+      FROM all_points
+      ;
+  END IF;
+  END;
 $$;
 
 CREATE OR REPLACE FUNCTION function_get_heirloom_tile_for_envelope(z integer, x integer, y integer, env_geom geometry, min_extent real)
@@ -560,7 +694,7 @@ CREATE OR REPLACE FUNCTION function_get_heirloom_tile_for_envelope(z integer, x 
         _area_3857 AS area_3857,
         _osm_type AS osm_type,
         _relation_ids AS relation_ids
-      FROM function_get_point_features(z, env_geom, power(min_extent * 32, 2)::real, power(min_extent * 4096, 2)::real, (min_extent * 192)::real, (min_extent * 192 * 16)::real)
+      FROM function_get_point_features(z, env_geom, ST_Expand(env_geom, ST_XMax(env_geom) - ST_XMin(env_geom)), power(min_extent * 32, 2)::real, power(min_extent * 4096, 2)::real, (min_extent * 192)::real, (min_extent * 192 * 16)::real)
     ),
     mvt_point_features AS (
       SELECT

ab46157712da31fa2e737085f04165df51c6d031
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2025-12-23 10:51:23 -0500

    Simplify low zoom a lot

--- server/sql/functions/get_line_features_for_tile.sql
+++ server/sql/functions/get_line_features_for_tile.sql
@@ -20,80 +20,8 @@ AS $$
     min_rel_extent := min_way_extent * 192;
     min_area := power(min_way_extent * 4, 2);
     simplify_tolerance := min_way_extent * 0.75;
-    IF z = 0 THEN
+    IF z <= 2 THEN
       RETURN;
-    ELSIF z <= 2 THEN
-      RETURN QUERY EXECUTE FORMAT($f$
-      WITH
-      way AS (
-          SELECT id, ''::hstore AS tags, geom
-          FROM untagged_way
-          WHERE geom && %2$L
-        UNION ALL
-          SELECT id, tags, geom
-          FROM way_explicit_area
-          WHERE geom && %2$L
-        UNION ALL
-          SELECT id, tags, geom
-          FROM way_explicit_line
-          WHERE geom && %2$L
-        UNION ALL
-          SELECT id, tags, geom
-          FROM way_no_explicit_geometry_type
-          WHERE geom && %2$L
-      ),
-      boundaries AS (
-        SELECT
-          id
-        FROM area_relation
-        WHERE geom && %2$L
-          AND tags @> 'boundary => administrative'
-          AND tags @> 'admin_level => 2'
-      ),
-      boundary_members AS (
-        SELECT
-          w.id AS id,
-          w.tags AS tags,
-          w.geom AS geom,
-          rw.member_role AS member_role,
-          r.id AS relation_id
-        FROM way w
-        JOIN way_relation_member rw ON w.id = rw.member_id
-        JOIN boundaries r ON rw.relation_id = r.id
-        WHERE NOT w.tags @> 'maritime => yes'
-      ),
-      collapsed AS (
-        SELECT
-          ANY_VALUE(tags) AS tags,
-          COALESCE(jsonb_object_agg('m.' || relation_id::text, member_role) FILTER (WHERE relation_id IS NOT NULL), '{}'::jsonb) AS membership_attributes,
-          ANY_VALUE(geom) AS geom,
-          ARRAY_AGG(relation_id) AS relation_ids
-        FROM boundary_members
-        GROUP BY id
-      ),
-      tagged AS (
-        SELECT
-          slice(tags, ARRAY[{{LOW_ZOOM_LINE_KEY_LIST}}])::jsonb || membership_attributes AS tags,
-          geom,
-          relation_ids
-        FROM collapsed
-      ),
-      grouped_and_simplified AS (
-        SELECT
-          tags,
-          ST_Simplify(ST_LineMerge(ST_Multi(ST_Collect(geom))), %6$L, true) AS geom,
-          ANY_VALUE(relation_ids) AS relation_ids
-        FROM tagged
-        GROUP BY tags
-      )
-      SELECT
-        NULL::int8 AS id,
-        tags,
-        geom,
-        relation_ids
-      FROM grouped_and_simplified
-      ;
-      $f$, z, env_geom, min_way_extent, min_rel_extent,  min_area, simplify_tolerance);
     ELSIF z < 12 THEN
       RETURN QUERY EXECUTE FORMAT($f$
       WITH
--- server/sql/functions/get_point_features_for_tile.sql
+++ server/sql/functions/get_point_features_for_tile.sql
@@ -27,7 +27,7 @@ AS $$
     z26_x_max := ((x + 1) * (1 << (26 - z))) - 1;
     z26_y_min := y * (1 << (26 - z));
     z26_y_max := ((y + 1) * (1 << (26 - z))) - 1;
-    IF z = 0 THEN
+    IF z <= 2 THEN
       RETURN QUERY EXECUTE FORMAT($f$
       SELECT
         id,
@@ -43,31 +43,6 @@ AS $$
       FROM node
       WHERE tags @> 'place => continent'
         OR tags @> 'place => ocean'
-      ;
-      $f$, z, env_geom, min_area, max_area, z26_x_min, z26_x_max, z26_y_min, z26_y_max);
-    ELSIF z <= 2 THEN
-      RETURN QUERY EXECUTE FORMAT($f$
-      SELECT
-        id,
-        (
-          SELECT jsonb_object_agg(key, value)
-          FROM jsonb_each(tags::jsonb)
-          WHERE key IN ({{POINT_KEY_LIST}}) {{POINT_KEY_PREFIX_LIKE_STATEMENTS}}
-        ) AS tags,
-        geom,
-        NULL::real AS area_3857,
-        'n' AS osm_type,
-        NULL::int8[] AS relation_ids
-      FROM node
-      WHERE 
-        z26_x BETWEEN %5$L AND %6$L
-        AND z26_y BETWEEN %7$L AND %8$L
-        AND (
-          tags @> 'place => continent'
-          OR tags @> 'place => ocean'
-          OR tags @> 'place => sea'
-          OR tags @> 'place => country'
-        )
       ;
       $f$, z, env_geom, min_area, max_area, z26_x_min, z26_x_max, z26_y_min, z26_y_max);
     ELSIF z < 12 THEN
